27cdc5283ff62ec30e7ab41e422b5ec6f0996bca
Premanshu Pandey <premanshuppandey@gmail.com>
2025-07-20 08:37:58 +0530

    WPABAI-45 - Token notification system and token info location updated (#12)
    * Token notification
    
    - Added token notification
    - Moved token info from license to Main Nav
    
    * Build
    
    ---------
    
    Co-authored-by: Navanath Bhosale <navanathb@bsf.io>

--- src/dashboard/App/Elements/Welcome/PostIdeas/PostIdeas.js
+++ src/dashboard/App/Elements/Welcome/PostIdeas/PostIdeas.js
@@ -130,11 +130,6 @@ export default function PostIdeas() {
 	}, [ postIdeas ] );
 
 	const handleRefresh = () => {
-		if ( ! wpaib_localized_data.pro_available ) {
-			window.open( wpaib_localized_data.upgrade_link, '_blank' );
-			return;
-		}
-
 		setPostIdeas( '' );
 		setLoading( true );
 		setError( null );
@@ -161,6 +156,25 @@ export default function PostIdeas() {
 	};
 
 	if ( error ) {
+		// Check for insufficient tokens error
+		if ( error === 'Token exhausted: Insufficient tokens for this request.' ) {
+			return (
+				<div className="p-4 text-red-500 flex flex-col items-center">
+					<p> { __( 'Error while loading post ideas:', 'wp-ai-blogger' ) } { error } </p>
+					<a
+						href="https://wpaiblogger.com/pricing/"
+						target="_blank"
+						rel="noopener noreferrer"
+						className="cursor-pointer inline-flex justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 gap-1 mt-5"
+						style={ { color: 'white' } }
+					>
+						{ __( 'Upgrade Now', 'wp-ai-blogger' ) }
+						<MoveRight className="h-5 w-5" />
+					</a>
+				</div>
+			);
+		}
+
 		if ( error === 'Missing required fields' ) {
 			const title = __( 'Please fill out the general settings to see post ideas.', 'wp-ai-blogger' );
 			const buttonText = __( 'Go to General Settings', 'wp-ai-blogger' );
@@ -288,7 +302,7 @@ export default function PostIdeas() {
 								<tfoot className="bg-gray-50">
 									<tr>
 										<td colSpan="5" className="px-3 py-3.5 text-center text-sm font-semibold text-indigo-600 hover:text-indigo-900">
-											<a href={ wpaib_localized_data.upgrade_link } className="text-indigo-600 hover:text-indigo-900 flex items-center justify-center gap-x-1">
+											<a href={ wpaib_localized_data.upgrade_link } target="_blank" className="text-indigo-600 hover:text-indigo-900 flex items-center justify-center gap-x-1" rel="noreferrer">
 												{ __( 'Upgrade to Pro to Unlock More Features.', 'wp-ai-blogger' ) }
 												<MoveRight className="w-5 h-5" />
 											</a>
