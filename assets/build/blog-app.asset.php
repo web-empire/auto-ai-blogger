cefee7e4491c55cfe56688f494376a83693fdb9b
Navanath Bhosale <navanathb@bsf.io>
2025-08-03 14:33:33 +0530

    Build fixes

--- assets/build/blog-app.asset.php
+++ assets/build/blog-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-i18n'), 'version' => '5f130c4cbbbccb8503f7');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-i18n'), 'version' => '258afbd5c8bc11f643a7');
