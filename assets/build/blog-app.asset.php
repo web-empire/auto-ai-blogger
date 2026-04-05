554c2dd5492af74f5869a3562c329c75159a8283
Navanath Bhosale <navanathb@bsf.io>
2026-04-05 17:23:29 +0530

    chore: Build

--- assets/build/blog-app.asset.php
+++ assets/build/blog-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-i18n'), 'version' => 'e0c6f97232a747ca43b9');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-i18n'), 'version' => 'd39478cf55445a852c34');
